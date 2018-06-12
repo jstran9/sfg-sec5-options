cd5e1c75c31cc6919d6f7404953e7bb52eba408a
todd <toddtran9@gmail.com>
2018-06-11 17:30:05 -0700

    Creating a new service that will be marked as a Primary bean.

--- src/main/java/guru/springframework/didemo/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/didemo/services/GreetingServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-    public static final String GREETING_MESSAGE = "Greeting from GreetingServiceImpl!";
+    public static final String GREETING_MESSAGE = "Greeting from GreetingServiceImpl! Hello Gurus!";
 
     @Override
     public String sayGreeting() {
